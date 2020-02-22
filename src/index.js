8e0275c909c685311d19e3af279ae0f64e3ffee1
Flavio Vinicius Miranda <vinniimiranda@gmail.com>
2020-02-22 18:29:27 -0300

    Update service worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import { registerServiceWorker } from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+serviceWorker.register();
