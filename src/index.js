6b7009c9e594eb1813b3bb60ba880c641d81c434
Flavio Vinicius Miranda <vinniimiranda@gmail.com>
2020-02-22 17:20:23 -0300

    Pwa

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { registerServiceWorker } from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+registerServiceWorker();
