c31765a5562ae7a2b6fdf632d8ece06b95b3c05b
Flavio Vinicius Miranda <vinniimiranda@gmail.com>
2020-02-22 18:08:09 -0300

    Pwa

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -12,5 +12,45 @@ export const registerServiceWorker = () => {
           // console.log('ServiceWorker registration failed: ', err);
         });
     });
+
+    window.addEventListener('install', function(event) {
+      console.log('WORKER: install event in progress.');
+      event.waitUntil(
+        /* The caches built-in is a promise-based API that helps you cache responses,
+         as well as finding and deleting them.
+      */
+        caches
+          /* You can open a cache by name, and this method returns a promise. We use
+           a versioned cache name here so that we can remove old cache entries in
+           one fell swoop later, when phasing out an older service worker.
+        */
+          .open(`v1::fundamentals`)
+          .then(function(cache) {
+            /* After the cache is opened, we can fill it with the offline fundamentals.
+             The method below will add all resources we've indicated to the cache,
+             after making HTTP requests for each of them.
+          */
+            return cache.addAll(['/', '/css/global.css', '/js/global.js']);
+          })
+          .then(function() {
+            console.log('WORKER: install completed');
+          })
+      );
+    });
+
+    window.addEventListener('install', event => {
+      console.log('👷', 'install', event);
+      window.skipWaiting();
+    });
+
+    window.addEventListener('activate', event => {
+      console.log('👷', 'activate', event);
+      return window.clients.claim();
+    });
+
+    window.addEventListener('fetch', function(event) {
+      // console.log('👷', 'fetch', event);
+      event.respondWith(fetch(event.request));
+    });
   }
 };
